91caabd32ae6c1bbc3393d7a461dbff10b1fe3fb
Martin Grabina <mgrabina@itba.edu.ar>
2017-05-09 20:28:24 -0300

    status

--- js/status.js
+++ js/status.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
-	
+	var select, aerolineas, long;
 	$.getJSON("http://hci.it.itba.edu.ar/v1/api/misc.groovy?method=getairlines",function(data){
-		var aerolineas = data.airlines;
-		var select= document.getElementsByTagName("select");		       	
-	    var long=aerolineas.length;
+		 aerolineas = data.airlines;
+		 select= document.getElementsByTagName("select");		       	
+	     long=aerolineas.length;
 	    for(var i=0; i<long;i++)
-	    	$("select").append("<option>"+data.airlines[i].name+"</option>");	
-	});	
+	    	$("select").append("<option value='"+ data.airlines[i].id+"'>"+data.airlines[i].name+"</option>");	
+	});		
 });
-	
+
+	function buscarEstado(){
+		var id = document.getElementsByTagName('select').value;
+		var number = document.getElementById('flightNumber').value; 
+		$.getJSON("http://hci.it.itba.edu.ar/v1/api/status.groovy?method=getflightstatus&airline_id="+id+"&flight_number="+number,function(data){
+			status = data.status;
+			window.alert(status);
+		});
+	}	
