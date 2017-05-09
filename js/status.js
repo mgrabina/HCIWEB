634959f14eba8921439b40fbd5493f1d832c1545
juan grethe <jgrethe@itba.edu.ar>
2017-05-09 18:29:54 -0300

    se agrega status

--- js/status.js
+++ js/status.js
@@ -1,9 +1,11 @@
-$(document).onReady({
-	$.getJSON( "http://hci.it.itba.edu.ar/v1/api/misc.groovy?method=getairlines", function( data ) {	
-    var datos = JSON.parse(data);
-    var select = document.getElementTagName("select");
-    for (let value of datos) {
- 		select.append('  <option value="'+ value.name+'">'+ value.name+'</option>')
-	}
+$(document).ready(function(){
+	
+	$.getJSON("http://hci.it.itba.edu.ar/v1/api/misc.groovy?method=getairlines",function(data){
+		var aerolineas = data.airlines;
+		var select= document.getElementsByTagName("select");		       	
+	    var long=aerolineas.length;
+	    for(var i=0; i<long;i++)
+	    	$("select").append("<option>"+data.airlines[i].name+"</option>");	
+	});	
 });
-});
+	
