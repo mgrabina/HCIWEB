2d0ea8a0a87925bb7ca737ebcc37479879430bdb
jcgrethe <jgrethe@itba.edu.ar>
2017-05-10 19:30:57 -0300

    se arregla features

--- js/featured.js
+++ js/featured.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
 	//Obtengo las ciudades
 	var select, cities, long;
-	$.getJSON("http://hci.it.itba.edu.ar/v1/api/misc.groovy?method=getcities",function(data){
+	select= document.getElementsByTagName("select");
+	$.getJSON("http://hci.it.itba.edu.ar/v1/api/geo.groovy?method=getcities",function(data){
 		 cities = data.cities;
-		 select= document.getElementsByTagName("select");		       	
+		 		       	
 	     long=cities.length;
 	    for(var i=0; i<long;i++)
 	    	$("select").append("<option value='"+ data.cities[i].id+"'>"+data.cities[i].name+"</option>");	
 	});
-	select.onchange = function(){
+	$("select").change(function(){
+		alert(select.value);
 		$.getJSON("http://hci.it.itba.edu.ar/v1/api/booking.groovy?method=getflightdeals&from="+select.value,function(data){
 		 var deals = data.deals;
 		 var table= document.getElementsByTagName("table");		       	
@@ -16,6 +18,6 @@ $(document).ready(function(){
 	    for(var i=0; i<long;i++)
 	    	$("table").append("<tr><td>"+deals[i].city.name+"</td><td>"+deals[i].city.country.name+"</td><td>"+deals[i].price+"</td></tr>");	
 	});
-	};
+	});
 
 });
