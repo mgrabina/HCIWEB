875036f056cc4c78c9bfaacdeb921f0d88e3c940
jcgrethe <jgrethe@itba.edu.ar>
2017-05-10 17:00:17 -0300

    se agrega validaciones en home
    se agrega validaciones en home

--- js/home2.js
+++ js/home2.js
@@ -19,23 +19,49 @@ $("#searchButton").click(validate);
 
 function validate(){
 
-
-
-  
-
-
-
-
-
 	//alert("is working fine");
 	var from = document.getElementById("from").value;
 	var to = document.getElementById("to").value;
-	var dateFrom = document.getElementById("dateFrom").value; // Format:  yyyy-mm-dd
-	var dateTo = document.getElementById("dateTo").value;
-	var roundTrip = $("#roundTrip").is(":checked");  // false or true
-	var adults = document.getElementById("adults").value;
-	var childs = document.getElementById("childs").value;
-	var infants = document.getElementById("infants").value;
+  if(from=="" || to=="")
+    alert("complete all the fields");
+  if(from==to){
+    alert("same city");
+  }
+  $.getJSON("http://hci.it.itba.edu.ar/v1/api/geo.groovy?method=getcities&page_size=100",function(data){
+
+      var long = data.total;
+      var flag= new Boolean(false);
+      var flag2=new Boolean(false);
+      for(var i=0;i<long && (flag==false || flag2==false) ;i++){
+        if(flag == false && from==data.cities[i].name){
+          flag=true;
+        }
+        if(flag2 == false && to==data.cities[i].name){
+          flag2=true;
+        }
+      }
+      if(flag2==false || flag== false){
+        alert("not a city");
+      }
+
+}).done(function(){
+  var dateFrom = document.getElementById("dateFrom").value; // Format:  yyyy-mm-dd
+  var dateTo = document.getElementById("dateTo").value;
+  if(dateFrom==dateTo){
+    alert("same date");
+   }
+
+  var roundTrip = $("#roundTrip").is(":checked");  // false or true
+  var adults = document.getElementById("adults").value;
+  var childs = document.getElementById("childs").value;
+  var infants = document.getElementById("infants").value;
+
+  if(adults==0 && childs==0 && infants == 0)
+    alert("no passengers");
+}); 
+
+
+	
 
 	//alert(dateFrom);
 
