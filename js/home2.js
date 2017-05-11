e755ee7c43e5026967ca827278d26a969ff9a5d7
jcgrethe <jgrethe@itba.edu.ar>
2017-05-11 11:15:04 -0300

    Add files via upload

--- js/home2.js
+++ js/home2.js
@@ -1,13 +1,35 @@
 
+var availableCities = [];
 $(document).ready(function(){
    //código a ejecutar cuando el DOM está listo para recibir instrucciones.
+  
 
+
+$.getJSON("http://hci.it.itba.edu.ar/v1/api/geo.groovy?method=getcities&page_size=100", function(data){
+
+      var size = data.total;
+      var i = 0;
+      for( i = 0; i<size; i++){
+        availableCities.push(data.cities[i].name);
+      }
+
+}); 
 	
-  checkCities();
+  checkCities(availableCities);
   datePicker();
 
 
 
+<<<<<<< HEAD
+var a=$("#searchButton").click(validate);
+if(a==false){
+  document.getElementById("searchButton").href="#";
+}
+else{
+ document.getElementById("searchButton").href="flightsList.html"; 
+}
+
+=======
 $("#searchButton").click(validate);
 
   document.getElementById("roundTrip").onchange = function(){
@@ -19,6 +41,7 @@ $("#searchButton").click(validate);
                 });
     }
   };
+>>>>>>> origin/master
 
 });
 
@@ -26,40 +49,44 @@ $("#searchButton").click(validate);
 
 function validate(){
 
+
 	//alert("is working fine");
 	var from = document.getElementById("from").value;
-	var to = document.getElementById("to").value;
-  if(from=="" || to=="")
+  var to = document.getElementById("to").value;
+  
+  if(from=="" || to==""){
     alert("complete all the fields");
-  if(from==to){
-    alert("same city");
+    ret=false;
   }
-  $.getJSON("http://hci.it.itba.edu.ar/v1/api/geo.groovy?method=getcities&page_size=100",function(data){
-
-      var long = data.total;
-      var flag= new Boolean(false);
-      var flag2=new Boolean(false);
-      for(var i=0;i<long && (flag==false || flag2==false) ;i++){
-        if(flag == false && from==data.cities[i].name){
-          flag=true;
-        }
-        if(flag2 == false && to==data.cities[i].name){
-          flag2=true;
-        }
-      }
-      if(flag2==false || flag== false){
-        alert("not a city");
-      }
-  });    
+  
+  if(from==to)
+    alert("same city");
+  var ret=true;
+  var long = availableCities.length;
+  var flag= false;
+  var flag2=false;
+  for(var i=0;i<long && (flag==false || flag2==false) ;i++){
+    if(flag == false && from==availableCities[i])
+      flag=true;
+    if(flag2 == false && to==availableCities[i])
+      flag2=true;
+  }
+  if(flag2==false || flag== false){
+    alert("not a city");
+    ret=false;
+  }
 
   var dateFrom = document.getElementById("dateFrom").value; // Format:  yyyy-mm-dd
   var dateTo = document.getElementById("dateTo").value;
   var time= new Date();
-  if(time<dateFrom || time<dateTo || dateTo<dateFrom)
+  if(time<dateFrom || time<dateTo || dateTo<dateFrom){
     alert("invalid day");
+    ret=false;
+  }
 
   if(dateFrom==dateTo){
     alert("same date");
+    ret=false;
    }
 
   var roundTrip = $("#roundTrip").is(":checked");  // false or true
@@ -67,39 +94,17 @@ function validate(){
   var childs = document.getElementById("childs").value;
   var infants = document.getElementById("infants").value;
 
-  if(adults==0 && childs==0 && infants == 0)
-    alert("no passengers");
-
-
-	
-
-	//alert(dateFrom);
-
-
-	
-
+  if(adults==0 && childs==0 && infants == 0){
+    alert("no passengers");  
+    ret=false;
+  }
+return ret;
+  
 
 }
 
 
-
 function checkCities(){
-  
-
-  var availableCities = [];
-
-
-$.getJSON("http://hci.it.itba.edu.ar/v1/api/geo.groovy?method=getcities&page_size=100",function(data){
-
-      var size = data.total;
-      var i = 0;
-      for( i = 0; i<size; i++){
-        availableCities.push(data.cities[i].name);
-      }
-
-}); 
-  
-
   $( function() {
     
 
