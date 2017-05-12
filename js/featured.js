395e934ddd23937d14f83e7dcd36e9cd510550b2
jcgrethe <jgrethe@itba.edu.ar>
2017-05-12 16:23:28 -0300

    se arregla features

--- js/featured.js
+++ js/featured.js
@@ -7,11 +7,25 @@ $(document).ready(function(){
 		 		       	
 	     long=cities.length;
 	    for(var i=0; i<long;i++)
-	    	$("select").append("<option value="+ data.cities[i].id+">"+data.cities[i].name+"</option>");	
+	    	$("select").append("<option value="+ data.cities[i].id+">"+data.cities[i].name+"</option>");
+	  for(var i=0; i<long;i++)
+	    	$("select").append("<option value="+ data.cities[i].id+">"+data.cities[i].name+"</option>");
+	    $("select").disabled=true;
+		$.getJSON("http://hci.it.itba.edu.ar/v1/api/booking.groovy?method=getflightdeals&from="+$("select").val(),function(data){
+			 var deals = data.deals;
+			 var table= document.getElementsByTagName("table");		       	
+		     long=deals.length;
+		     $("table").append("<tr id='primeraFila'><td>City</td><td>Country</td><td>Price</td></tr>");
+		    for(var i=0; i<long;i++)
+		    	$("table").append("<tr id='fila'><td>"+deals[i].city.name+"</td><td>"+deals[i].city.country.name+"</td><td>"+deals[i].price+"</td></tr>");	
+	}).done(function(){
+		$("select").removeAttr("disabled");  	
+	});		
 	});
 	
 	$("select").change(function(){
 		$("table").children().remove();
+		this.disabled=true;
 		$.getJSON("http://hci.it.itba.edu.ar/v1/api/booking.groovy?method=getflightdeals&from="+this[this.selectedIndex].value,function(data){
 		 var deals = data.deals;
 		 var table= document.getElementsByTagName("table");		       	
@@ -19,6 +33,8 @@ $(document).ready(function(){
 	     $("table").append("<tr id='primeraFila'><td>City</td><td>Country</td><td>Price</td></tr>");
 	    for(var i=0; i<long;i++)
 	    	$("table").append("<tr id='fila'><td>"+deals[i].city.name+"</td><td>"+deals[i].city.country.name+"</td><td>"+deals[i].price+"</td></tr>");	
+	}).done(function(){
+		$("select").removeAttr("disabled");
 	});
 	});
 
