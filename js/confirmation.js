2ce0902403299af1a20f2ac604bbaabce60c88a2
Martin Grabina <mgrabina@itba.edu.ar>
2017-05-18 17:36:00 -0300

    as

--- js/confirmation.js
+++ js/confirmation.js
@@ -19,6 +19,22 @@ var infants = sessionStorage.getItem("infants");
 
 
 function book(){
+  var flight_id;
+  var installments;
+  var credit_card_number;
+  var expiration;
+  var credit_card_first_name;
+  var credit_card_last_name;
+  var security_code;
+  var city_id;
+  var city_state;
+  var country_id;
+  var zipcode;
+  var street;
+  var apartment;
+  var floor;
+  var email;
+  var phone;
   var url = 'http://hci.it.itba.edu.ar/v1/api/booking.groovy?method=bookflight2&booking=%7b%22flight_id%22:'+flight_id+',%22passengers%22:%5b%7b%22first_name%22:%22John%22,%22last_name%22:%22Doe%22,%22birthdate%22:%221969-06-02%22,%22id_type%22:1,%22id_number%22:%2217155171%22%7d%5d,%22payment%22:%7b%22installments%22:'+installments+',%22credit_card%22:%7b%22number%22:%'+credit_card_number+'%22,%22expiration%22:%22'+expiration+'%22,%22security_code%22:%22'+security_code+'%22,%22first_name%22:%22'+credit_card_first_name+'%22,%22last_name%22:%22'+credit_card_last_name+'%22%7d,%22billing_address%22:%7b%22city%22:%7b%22id%22:%22'+city_id+'%22,%22state%22:%22'+city_state+'%22,%22country%22:%7b%22id%22:%22'+country_id+'%22%7d%7d,%22zip_code%22:'+zipcode+',%22street%22:%22'+street+'%22,%22floor%22:%22'+floor+'%22,%22apartment%22:%22'+apartment+'%22%7d%7d,%22contact%22:%7b%22email%22:%22'+email+'%22,%22phones%22:%5b%22'+phone+'%22%5d%7d%7d';
   $.getJSON(url, function(data){
     if(data.booking){
